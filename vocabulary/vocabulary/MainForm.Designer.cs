ba1cfa14f9fbe7a1adc1281e99b02f2d13c00f00
adzen <101adzen@gmail.com>
2013-02-04 02:38:33 +0800

    Open dictionary file. (Encoding is UTF-8)

--- vocabulary/vocabulary/MainForm.Designer.cs
+++ vocabulary/vocabulary/MainForm.Designer.cs
@@ -28,12 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.openbutton = new System.Windows.Forms.Button();
+            this.dictionOpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // openbutton
+            // 
+            this.openbutton.Location = new System.Drawing.Point(12, 12);
+            this.openbutton.Name = "openbutton";
+            this.openbutton.Size = new System.Drawing.Size(75, 23);
+            this.openbutton.TabIndex = 0;
+            this.openbutton.Text = "Open File...";
+            this.openbutton.UseVisualStyleBackColor = true;
+            this.openbutton.Click += new System.EventHandler(this.openbutton_Click);
+            // 
+            // dictionOpenFileDialog
+            // 
+            this.dictionOpenFileDialog.FileName = "openFileDialog1";
+            this.dictionOpenFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.dictionOpenFileDialog_FileOk);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 266);
+            this.Controls.Add(this.openbutton);
+            this.Name = "MainForm";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button openbutton;
+        private System.Windows.Forms.OpenFileDialog dictionOpenFileDialog;
     }
 }
 
--- vocabulary/vocabulary/MainForm.cs
+++ vocabulary/vocabulary/MainForm.cs
@@ -6,13 +6,76 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 
+using System.IO;
+
 namespace vocabulary
 {
     public partial class MainForm : Form
     {
+        public List<word> dictionary = new List<word>();
+
         public MainForm()
         {
             InitializeComponent();
         }
+
+        private void loadData(string filename)
+        {
+            string[] lines = File.ReadAllLines(filename, Encoding.UTF8);
+            if(lines.Length <= 0)
+            {
+                MessageBox.Show(@"It's an empty file!");
+                return;
+            }
+            dictionary.Clear();
+
+            int newWordCount = 0;
+            foreach (string line in lines)
+            {
+                string[] tokens = line.Split(new char[] { '\t' });
+                if (tokens.Length != 4 && tokens.Length != 2) 
+                {
+                    MessageBox.Show(@"Invalid input format!");
+                    return;
+                }
+
+                if (tokens.Length == 4)
+                {
+                    word aword = new word();
+                    aword.righted = int.Parse(tokens[0]);
+                    aword.questioned = int.Parse(tokens[1]);
+                    aword.english = tokens[2];
+                    aword.chinese = tokens[3];
+                    dictionary.Insert(0, aword);                    
+                }
+                else
+                {
+                    word aword = new word();
+                    aword.righted = aword.questioned = 0;
+                    aword.english = tokens[0];
+                    aword.chinese = tokens[1];
+                    dictionary.Insert(0, aword);
+                    newWordCount++;
+                }
+            }
+
+            MessageBox.Show(@"Total = " + dictionary.Count.ToString() + @", New Word = " + newWordCount.ToString());
+        }
+
+        private void dictionOpenFileDialog_FileOk(object sender, CancelEventArgs e)
+        {
+            loadData(dictionOpenFileDialog.FileName);
+        }
+
+        private void openbutton_Click(object sender, EventArgs e)
+        {
+            dictionOpenFileDialog.ShowDialog();
+        }
+    }
+
+    public class word
+    {
+        public string english, chinese;
+        public int questioned, righted;
     }
 }
