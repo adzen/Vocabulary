2b6b5c5650765b55bd7d521728d86802cb38a7e3
adzen <101adzen@gmail.com>
2013-02-05 06:32:04 +0800

    Almost done... :D

--- vocabulary/vocabulary/MainForm.Designer.cs
+++ vocabulary/vocabulary/MainForm.Designer.cs
@@ -30,6 +30,14 @@
         {
             this.openbutton = new System.Windows.Forms.Button();
             this.dictionOpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.problemText = new System.Windows.Forms.Label();
+            this.startbutton = new System.Windows.Forms.Button();
+            this.stopbutton = new System.Windows.Forms.Button();
+            this.option1 = new System.Windows.Forms.Button();
+            this.option2 = new System.Windows.Forms.Button();
+            this.option3 = new System.Windows.Forms.Button();
+            this.option4 = new System.Windows.Forms.Button();
+            this.statusLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // openbutton
@@ -45,16 +53,118 @@
             // dictionOpenFileDialog
             // 
             this.dictionOpenFileDialog.FileName = "openFileDialog1";
+            this.dictionOpenFileDialog.Title = "開啟字彙列表";
             this.dictionOpenFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.dictionOpenFileDialog_FileOk);
+            // 
+            // problemText
+            // 
+            this.problemText.Enabled = false;
+            this.problemText.Font = new System.Drawing.Font("微軟正黑體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.problemText.Location = new System.Drawing.Point(12, 38);
+            this.problemText.Name = "problemText";
+            this.problemText.Size = new System.Drawing.Size(345, 38);
+            this.problemText.TabIndex = 1;
+            this.problemText.Text = "題目";
+            this.problemText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // startbutton
+            // 
+            this.startbutton.Enabled = false;
+            this.startbutton.Location = new System.Drawing.Point(93, 12);
+            this.startbutton.Name = "startbutton";
+            this.startbutton.Size = new System.Drawing.Size(75, 23);
+            this.startbutton.TabIndex = 2;
+            this.startbutton.Text = "Start";
+            this.startbutton.UseVisualStyleBackColor = true;
+            this.startbutton.Click += new System.EventHandler(this.startbutton_Click);
+            // 
+            // stopbutton
+            // 
+            this.stopbutton.Enabled = false;
+            this.stopbutton.Location = new System.Drawing.Point(174, 12);
+            this.stopbutton.Name = "stopbutton";
+            this.stopbutton.Size = new System.Drawing.Size(75, 23);
+            this.stopbutton.TabIndex = 3;
+            this.stopbutton.Text = "Stop";
+            this.stopbutton.UseVisualStyleBackColor = true;
+            this.stopbutton.Click += new System.EventHandler(this.stopbutton_Click);
+            // 
+            // option1
+            // 
+            this.option1.Enabled = false;
+            this.option1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.option1.Location = new System.Drawing.Point(12, 79);
+            this.option1.Name = "option1";
+            this.option1.Size = new System.Drawing.Size(345, 41);
+            this.option1.TabIndex = 4;
+            this.option1.Text = "選項1";
+            this.option1.UseVisualStyleBackColor = true;
+            this.option1.Click += new System.EventHandler(this.option1_Click);
+            // 
+            // option2
+            // 
+            this.option2.Enabled = false;
+            this.option2.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.option2.Location = new System.Drawing.Point(12, 126);
+            this.option2.Name = "option2";
+            this.option2.Size = new System.Drawing.Size(345, 41);
+            this.option2.TabIndex = 4;
+            this.option2.Text = "選項2";
+            this.option2.UseVisualStyleBackColor = true;
+            this.option2.Click += new System.EventHandler(this.option2_Click);
+            // 
+            // option3
+            // 
+            this.option3.Enabled = false;
+            this.option3.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.option3.Location = new System.Drawing.Point(12, 173);
+            this.option3.Name = "option3";
+            this.option3.Size = new System.Drawing.Size(345, 41);
+            this.option3.TabIndex = 4;
+            this.option3.Text = "選項3";
+            this.option3.UseVisualStyleBackColor = true;
+            this.option3.Click += new System.EventHandler(this.option3_Click);
+            // 
+            // option4
+            // 
+            this.option4.Enabled = false;
+            this.option4.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.option4.Location = new System.Drawing.Point(12, 220);
+            this.option4.Name = "option4";
+            this.option4.Size = new System.Drawing.Size(345, 41);
+            this.option4.TabIndex = 4;
+            this.option4.Text = "選項4";
+            this.option4.UseVisualStyleBackColor = true;
+            this.option4.Click += new System.EventHandler(this.option4_Click);
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.statusLabel.Location = new System.Drawing.Point(257, 9);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(100, 23);
+            this.statusLabel.TabIndex = 5;
+            this.statusLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 266);
+            this.ClientSize = new System.Drawing.Size(369, 274);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.option4);
+            this.Controls.Add(this.option3);
+            this.Controls.Add(this.option2);
+            this.Controls.Add(this.option1);
+            this.Controls.Add(this.stopbutton);
+            this.Controls.Add(this.startbutton);
+            this.Controls.Add(this.problemText);
             this.Controls.Add(this.openbutton);
+            this.MaximizeBox = false;
             this.Name = "MainForm";
-            this.Text = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "單字測驗程式";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
             this.ResumeLayout(false);
 
         }
@@ -63,6 +173,14 @@
 
         private System.Windows.Forms.Button openbutton;
         private System.Windows.Forms.OpenFileDialog dictionOpenFileDialog;
+        private System.Windows.Forms.Label problemText;
+        private System.Windows.Forms.Button startbutton;
+        private System.Windows.Forms.Button stopbutton;
+        private System.Windows.Forms.Button option1;
+        private System.Windows.Forms.Button option2;
+        private System.Windows.Forms.Button option3;
+        private System.Windows.Forms.Button option4;
+        private System.Windows.Forms.Label statusLabel;
     }
 }
 
--- vocabulary/vocabulary/MainForm.cs
+++ vocabulary/vocabulary/MainForm.cs
@@ -13,6 +13,10 @@ namespace vocabulary
     public partial class MainForm : Form
     {
         public List<word> dictionary = new List<word>();
+        public int answered = 0, corrected = 0;
+        public int[] optionId = new int[4]{0,0,0,0};
+        public int selectId = 0, rightedId = 0;
+        public Random rnd = new Random();
 
         public MainForm()
         {
@@ -42,11 +46,11 @@ namespace vocabulary
                 if (tokens.Length == 4)
                 {
                     word aword = new word();
-                    aword.righted = int.Parse(tokens[0]);
-                    aword.questioned = int.Parse(tokens[1]);
+                    aword.questioned = int.Parse(tokens[0]);
+                    aword.righted = int.Parse(tokens[1]);
                     aword.english = tokens[2];
                     aword.chinese = tokens[3];
-                    dictionary.Insert(0, aword);                    
+                    dictionary.Insert(dictionary.Count, aword);                    
                 }
                 else
                 {
@@ -54,12 +58,13 @@ namespace vocabulary
                     aword.righted = aword.questioned = 0;
                     aword.english = tokens[0];
                     aword.chinese = tokens[1];
-                    dictionary.Insert(0, aword);
+                    dictionary.Insert(dictionary.Count, aword);
                     newWordCount++;
                 }
             }
 
             MessageBox.Show(@"Total = " + dictionary.Count.ToString() + @", New Word = " + newWordCount.ToString());
+            startbutton.Enabled = true;
         }
 
         private void dictionOpenFileDialog_FileOk(object sender, CancelEventArgs e)
@@ -71,11 +76,130 @@ namespace vocabulary
         {
             dictionOpenFileDialog.ShowDialog();
         }
+
+        private void updateText()
+        {
+            problemText.Text = dictionary[selectId].english;
+            option1.Text = dictionary[optionId[0]].chinese;
+            option2.Text = dictionary[optionId[1]].chinese;
+            option3.Text = dictionary[optionId[2]].chinese;
+            option4.Text = dictionary[optionId[3]].chinese;
+            statusLabel.Text = corrected.ToString() + @" / " + answered.ToString();
+        }
+
+        private void randNewProblem()
+        {
+            rightedId = rnd.Next(4);
+            for (int i = 0; i < 4; ++i)
+            {
+                dictionary[optionId[i]].select = false;
+                optionId[i] = -1;
+            }
+            optionId[rightedId] = selectId = rnd.Next(dictionary.Count);
+            dictionary[selectId].select = true;
+            for (int i = 0; i < 4; ++i)
+            {
+                if (optionId[i] < 0)
+                {
+                    while (true)
+                    {
+                        optionId[i] = rnd.Next(dictionary.Count);
+                        if (dictionary[optionId[i]].select == false) 
+                        {
+                            dictionary[optionId[i]].select = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            updateText();
+        }
+
+        private void startbutton_Click(object sender, EventArgs e)
+        {
+            openbutton.Enabled = startbutton.Enabled = false;
+            stopbutton.Enabled = problemText.Enabled = 
+                option1.Enabled = option2.Enabled = 
+                option3.Enabled = option4.Enabled = true;
+            randNewProblem();
+        }
+
+        private void stopbutton_Click(object sender, EventArgs e)
+        {
+            startbutton.Enabled = true;
+            stopbutton.Enabled = problemText.Enabled =
+                option1.Enabled = option2.Enabled =
+                option3.Enabled = option4.Enabled = false;
+        }
+
+        private void option1_Click(object sender, EventArgs e)
+        {
+            answered++;
+            dictionary[selectId].questioned++;
+            if (rightedId == 0)
+            {
+                corrected++;
+                dictionary[selectId].righted++;
+            }
+            randNewProblem();
+        }
+
+        private void option2_Click(object sender, EventArgs e)
+        {
+            answered++;
+            dictionary[selectId].questioned++;
+            if (rightedId == 1)
+            {
+                corrected++;
+                dictionary[selectId].righted++;
+            }
+            randNewProblem();
+        }
+
+        private void option3_Click(object sender, EventArgs e)
+        {
+            answered++;
+            dictionary[selectId].questioned++;
+            if (rightedId == 2)
+            {
+                corrected++;
+                dictionary[selectId].righted++;
+            }
+            randNewProblem();
+        }
+
+        private void option4_Click(object sender, EventArgs e)
+        {
+            answered++;
+            dictionary[selectId].questioned++;
+            if (rightedId == 3)
+            {
+                corrected++;
+                dictionary[selectId].righted++;
+            }
+            randNewProblem();
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (dictionary.Count <= 0) return;
+
+            string[] output = new string[dictionary.Count];
+            for(int i = 0; i < dictionary.Count; ++i) 
+            {
+                word w = dictionary[i];
+                output[i] = w.questioned.ToString() + "\t" + w.righted.ToString() + "\t"
+                    + w.english + "\t" + w.chinese; 
+            }
+
+            File.WriteAllLines(dictionOpenFileDialog.FileName, output, Encoding.UTF8);
+        }
     }
 
     public class word
     {
         public string english, chinese;
         public int questioned, righted;
+        public bool select = false;
     }
 }
